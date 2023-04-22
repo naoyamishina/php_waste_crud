d043e4cefc0e311e9a68929247124f25b7784afe
mishina naoya <msnyxx811@gmail.com>
2023-04-23 07:47:00 +0900

    post投稿のバリデーション作成

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -3,6 +3,8 @@
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
           投稿の新規作成
       </h2>
+      <x-validation-errors class="mb-4" :errors="$errors" />
+
       @if(session('message'))
           <x-message :message="session('message')" />
       @endif
@@ -14,13 +16,14 @@
                   <div class="md:flex items-center mt-8">
                       <div class="w-full flex flex-col">
                       <label for="body" class="font-semibold leading-none mt-4">件名</label>
-                      <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" placeholder="Enter Title">
+                      <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{old('title')}}" placeholder="Enter Title">
                       </div>
                   </div>
   
                   <div class="w-full flex flex-col">
                       <label for="body" class="font-semibold leading-none mt-4">本文</label>
-                      <textarea name="body" class="w-auto py-2 border border-gray-300 rounded-md" id="body" cols="30" rows="10"></textarea>
+                      <textarea name="body" class="w-auto py-2 border border-gray-300 rounded-md" id="body" cols="30" rows="10">
+                      {{old('body')}}</textarea>
                   </div>
   
                   <div class="w-full flex flex-col">
