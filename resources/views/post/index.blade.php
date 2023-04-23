08aa88671b82d5560888c32dfd5c80870de1471e
mishina naoya <msnyxx811@gmail.com>
2023-04-23 15:19:42 +0900

    検索ボタンの設置

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,11 +3,24 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             投稿の一覧
         </h2>
-
         <x-message :message="session('message')" />
-
     </x-slot>
 
+    <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <form method="GET" action="{{ route('post.index') }}">
+            <div class="md:flex items-center mt-8">
+                <div class="flex">
+                    <input type="search" class="w-auto py-2 placeholder-gray-400 border border-gray-300 rounded-md" placeholder="入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
+                    <button class="bg-gray-300 mx-4 p-2 rounded-md" type="submit">検索</button>
+                    <button class="bg-gray-300 p-2 rounded-md">
+                        <a href="{{ route('post.index') }}" class="">
+                            クリア
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
     {{-- 投稿一覧表示用のコード --}}
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         @if (count($posts) == 0)
--- resources/views/post/mypost.blade.php
+++ resources/views/post/mypost.blade.php
@@ -18,7 +18,7 @@
         @foreach ($posts as $post)
             <div class="mx-4 sm:p-8">
                 <div class="mt-4">
-                    <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
+                    <div class="bg-white w-full rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
                         <div class="mt-4">
                             <h1 class="text-lg text-gray-700 font-semibold hover:underline cursor-pointer float-left">
                                 <a href="{{route('post.show', $post)}}">{{ $post->title }}</a>
